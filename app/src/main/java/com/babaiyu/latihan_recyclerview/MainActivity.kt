b5b2d82f3b331c01d5816704f5d6928f08946d5b
Babaiyu <babaiyu@Babaiyus-MacBook-Pro.local>
2023-09-17 17:18:45 +0700

    Create example of RecyclerView

---
+++ app/src/main/java/com/babaiyu/latihan_recyclerview/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.babaiyu.latihan_recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MinumanAdapter(private val dataSet: Array<String>) :
+    RecyclerView.Adapter<MinumanAdapter.ViewHolder>() {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView
+
+        init {
+            textView = view.findViewById(R.id.textView)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.text_row_item, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = dataSet[position]
+    }
+
+    override fun getItemCount() = dataSet.size
+}
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val dataSet = arrayOf(
+            "Januari",
+            "Februari",
+            "Maret",
+            "April",
+            "Mei",
+            "Juni",
+            "Juli",
+            "Agustus",
+            "September",
+            "Oktober",
+            "November",
+            "Desember",
+            "Januari-1",
+            "Februari-2",
+            "Maret-3",
+            "April-4",
+            "Mei-5",
+            "Juni-6",
+            "Juli-7",
+            "Agustus-8",
+            "September-9",
+            "Oktober-10",
+            "November-11",
+            "Desember-12",
+        )
+        val adapter = MinumanAdapter(dataSet)
+
+        val rvBulan: RecyclerView = findViewById(R.id.rv_month)
+        rvBulan.layoutManager = LinearLayoutManager(this)
+        rvBulan.adapter = adapter
+    }
+}
